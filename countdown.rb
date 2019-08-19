3432c8220576a3997cfb86dfd1cef946f024763d
Ashley Norton <nortonashleym@gmail.com>
2019-08-19 03:16:12 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,8 +3,8 @@
 def countdown (number)
   while number > 0 
     puts "#{number} SECOND(S)!"
-    number -= 1
     countdown_with_sleep(number)
+    number -= 1
   end
   return "HAPPY NEW YEAR!"
 end
